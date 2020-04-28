593b9d62e476c80bc552798f0d889ee858f9ad61
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-04-28 20:10:40 +0000

    learning python

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+b = 0
+c = 0
+for b in range (0, 10):
+    c = b + 1
+    for c in range (c, 10):
+        if (b != 8 or c != 9):
+            print("{:0d}{:0d}".format(b, c), end=", ")
+        else:
+            print("{}{}".format(b, c))
