9bb6c197460ac0f324f6658aba7f169791e72bda
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-01 00:35:29 +0000

    learning python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,20 +4,25 @@ if __name__ == "__main__":
     import sys
     if (len(sys.argv) - 1 < 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     else:
         if (sys.argv[2] == '+'):
             if (len(sys.argv) - 1 > 1):
                 print("{} + {} = {}".format(sys.argv[1], sys.argv[3],\
                     (add(int(sys.argv[1]), int(sys.argv[3])))))
+                exit(0)
         if (sys.argv[2] == '-'):
             if (len(sys.argv) - 1 > 1):
                 print("{} + {} = {}".format(sys.argv[1], sys.argv[3],\
                     (sub(int(sys.argv[1]), int(sys.argv[3])))))
+                exit(0)
         if (sys.argv[2] == '*'):
             if (len(sys.argv) - 1 > 1):
                 print("{} + {} = {}".format(sys.argv[1], sys.argv[3],\
                     (mul(int(sys.argv[1]), int(sys.argv[3])))))
+                exit(0)
         if (sys.argv[2] == '/'):
             if (len(sys.argv) - 1 > 1):
                 print("{} + {} = {}".format(sys.argv[1], sys.argv[3],\
                     (div(int(sys.argv[1]), int(sys.argv[3])))))
+                exit(0)
