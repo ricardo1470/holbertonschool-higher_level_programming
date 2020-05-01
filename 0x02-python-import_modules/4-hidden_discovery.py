ab3805fba77e9cd9c729d8bd25ae843229e959d0
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-01 03:17:59 +0000

    learning python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for hidd in dir(hidden_4):
-        if (hidd != '_'):
-            print("{}".format(hidd))
+        if (hidd[0] != '_'):
+            print("{0}".format(hidd))
