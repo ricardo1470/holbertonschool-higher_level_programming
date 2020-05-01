88e2e8a2229a234c92a49701a224b273f4cfb593
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-01 01:28:16 +0000

    learning python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,22 +5,25 @@ if __name__ == "__main__":
     if (len(sys.argv) - 1 != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    if (sys.argv[2] != '+', '-', '*', '/'):
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
     else:
         nun = int(sys.argv[1])
         nun2 = int(sys.argv[3])
         if (sys.argv[2] == '+'):
-            if (len(sys.argv) - 1 > 1):
+            if (len(sys.argv) - 1 >= 3):
                 print("{} + {} = {}".format(nun, nun2, (add(nun, nun2))))
                 exit(0)
         if (sys.argv[2] == '-'):
-            if (len(sys.argv) - 1 > 1):
+            if (len(sys.argv) - 1 >= 3):
                 print("{} - {} = {}".format(nun, nun2, (sub(nun, nun2))))
                 exit(0)
         if (sys.argv[2] == '*'):
-            if (len(sys.argv) - 1 > 1):
+            if (len(sys.argv) - 1 >= 3):
                 print("{} * {} = {}".format(nun, nun2, (mul(nun, nun2))))
                 exit(0)
         if (sys.argv[2] == '/'):
-            if (len(sys.argv) - 1 > 1):
+            if (len(sys.argv) - 1 >= 3):
                 print("{} / {} = {}".format(nun, nun2, (div(nun, nun2))))
                 exit(0)
