66de5eae8fbd9f822213b4626f3af7d78afbb1e8
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-05 23:53:36 +0000

    learning python

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/bin/bash/python3
 def no_c(my_string):
     for ch in my_string:
-        if (ch != 'c' or ch != 'C'):
-            i = my_string
-            return(i.translate({ord('c'): None, ord('C'): None}))
+        i = my_string
+        return(i.translate({ord('c'): None, ord('C'): None}))
