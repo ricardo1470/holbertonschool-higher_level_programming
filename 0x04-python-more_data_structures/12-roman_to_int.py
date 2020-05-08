fb724055ad605cf9a76bfcde8f74af9cde96edad
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-08 04:23:31 +0000

    create

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string and type(roman_string) == str:
+        rn = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        nun_rn = [s for s in roman_string]
+        res = 0
+        l = len(nun_rn)
+        for i in range(l):
+            if i < l - 1 and rn.get(nun_rn[i]) < rn.get(nun_rn[i + 1]):
+                res -= rn.get(nun_rn[i])
+            else:
+                res += rn.get(nun_rn[i])
+        return (res)
+    return (0)
