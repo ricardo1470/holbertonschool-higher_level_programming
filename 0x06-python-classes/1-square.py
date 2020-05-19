88a84e9313485c0b46cdd3f5792eee0fd4c3a478
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-19 22:11:52 +0000

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create a Squeare class"""
+
+
+class Square:
+    """Square class with a construction method"""
+    def __init__(self, size=0):
+        """Initialize Square with size attribute"""
+        self.__size = size
