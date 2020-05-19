4496d6d862589eba73cf1445aefd19ffaa9cc652
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-19 00:47:54 +0000

    create

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        i = a / b
+    except:
+        i = None
+    finally:
+        print("Inside result:{}".format(i))
+    return i
