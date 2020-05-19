bdcda94cdbc77f8036e2ab387ba08366f2a46c67
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-19 22:14:46 +0000

    update

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Create a Square class"""
+
+
+class Square:
+    """Square class with a construction method"""
+    def __init__(self, size=0):
+        """Initialize Square with size attribute"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
