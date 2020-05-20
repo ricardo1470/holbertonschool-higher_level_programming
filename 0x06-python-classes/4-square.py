6465b231cd7a0b17ed5a36ca46d03aba695435b8
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-20 00:04:53 +0000

    update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,15 +7,22 @@ class Square:
     def __init__(self, size=0):
         """Initialize Square"""
         self.__size = size
-        if type(size) is not int:
+        """if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")"""
 
     def area(self, area=0):
         """defines area"""
         return(self.__size * self.__size)
+    
+    @property
+    def size(self):
 
+        """ define size"""
+        return self.__size
+
+    @size.setter
     def size(self, value):
         """Define area"""
         if type(value) is not int:
