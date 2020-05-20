04924b2a4e70ff64d83f8f8fc537e101e57069fa
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-20 00:11:27 +0000

    update

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Create class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """Initialize Square"""
+        self.__size = size
+        """if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")"""
+
+    def area(self, area=0):
+        """defines area"""
+        return(self.__size * self.__size)
+
+    @property
+    def size(self):
+
+        """ define size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Define area"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """print Square"""
+        size = self.__size
+        if size is not 0:
+            for column in range(size):
+                for row in range(size):
+                    print("#", end="")
+                print("")
+        else:
+            print("")
