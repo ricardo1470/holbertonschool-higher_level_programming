d3e2784da6a3bab1eb8a3b8319eb9425b4018b15
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-23 07:59:55 +0000

    update

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,10 +6,12 @@ def text_indentation(text):
     """text indentation """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    for i in range(len(text)):
+    i = 0
+    while i < len(text):
         if text[i] not in [".", "?", ":"]:
-            if (text[i] != " " and text[i] != "    " or
-               (text[i - 1] and text[i - 1] not in [".", "?", ":"])):
-                print(text[i], end='')
+            print(text[i], end='')
         else:
             print('{}\n'.format(text[i]))
+            while (i + 1) < len(text) and ord(text[i + 1]) == 32:
+                i += 1
+        i += 1
