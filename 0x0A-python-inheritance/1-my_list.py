0a4cd7441c8db9f6c774e9224d6e9e2d81798a9a
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-06-03 02:47:02 +0000

    update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,6 +2,10 @@
 class MyList(list):
     """class list."""
 
+    def __init__(self):
+        """ initializes object """
+        pass
+
     def print_sorted(self):
         """ attributes and methods class"""
         print(sorted(self))
