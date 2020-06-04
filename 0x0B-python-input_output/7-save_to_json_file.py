f37f97607b975c63aa3bc3bd7da8f6ea8d028019
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-06-04 00:57:19 +0000

    update

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""create"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+
+    with open(filename, "w") as json_file:
+        json_file.write(json.dumps(my_obj))
+    json_file.close()
