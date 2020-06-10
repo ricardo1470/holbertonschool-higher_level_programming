8562d39836483e4cfb371afdae080ffdfa24cbab
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-06-10 02:16:23 +0000

    update files

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,7 +88,7 @@ class Rectangle(Base):
         """update the class Rectangle"""
         if (self.y > 0):
             for i in range(self.y):
-                print("\n" * self.y, end="")
+                print("")
         for i in range(self.height):
             print(" " * self.x, "#" * self.width)
 
