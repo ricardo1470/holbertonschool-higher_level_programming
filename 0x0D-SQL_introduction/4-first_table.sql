9fca12cbed320a1aed836e6bbda3185f2bf96540
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-07-13 15:16:37 -0500

    update file, create table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- script that creates a table called first_table
+-- in the current database in your MySQL server.
+DROP TABLE  IF EXISTS firts_table;
+CREATE TABLE first_table (
+id INT,
+name VARCHAR(256));
