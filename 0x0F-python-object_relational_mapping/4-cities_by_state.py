dea8fc498596bfd14ee49261501951d4b56e57e4
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-08 13:11:12 +0000

    update file

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-""" script that takes in arguments
-    and displays all values in the states
-    table of hbtn_0e_0_usa where name matches
-    the argument. But this time, write one that
-    is safe from MySQL injections!"""
+""" script that lists all cities
+    from the database hbtn_0e_4_usa """
 
 
 from sys import argv
