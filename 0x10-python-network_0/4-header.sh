625090c1f8538ee5440d2f0d3780b8ab8d925cd2
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-19 01:46:27 +0000

    script that takes in a URL as an argument
    sends a GET request to the URL
    
    and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sH "$1" "X-HolbertonSchool-User-Id: 98"
+curl "$1" -sH "X-HolbertonSchool-User-Id: 98"
