eb2ec4822ebc48aee9fc369b67dfbe836bbb1139
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-24 06:52:44 -0500

    update file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+from urllib import request 
+""" no se que comentar """
+
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    with request.urlopen(url) as reponse:
+        req_pag = reponse.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(req_pag)))
+        print("\t- content: {}".format(req_pag))
+        print("\t- utf8 content: {}".format(req_pag.decode('utf-8')))
