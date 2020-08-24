48d4faef773ea504eea6af703c97dea9e52d81e9
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-24 14:47:18 -0500

    update file
    send a post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     send_email = {"email": argv[2]}
     post_pag = parse.urlencode(send_email).encode("utf-8")
     req_req = request.Request(req_pag, post_pag)
-    
+
     with request.urlopen(req_req) as response:
         data = response.read().decode("utf-8")
-    print (data)
+    print(data)
