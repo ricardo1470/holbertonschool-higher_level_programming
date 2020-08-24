efb12985f37bdb0346366cb83ba9200bfa077779
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-24 14:05:05 -0500

    update file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import urllib.request
 """ script that fetches https://intranet.hbtn.io/status
     -You must use the package urllib
     -You must use a with statement """
+import urllib.request
 
 
 if __name__ == '__main__':
-    #url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
         req_pag = response.read()
     print("Body response:")
     print("\t- type: {}".format(type(req_pag)))
