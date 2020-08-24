70b8674774a8c3d8c2f16d8bb9619d0e2cfdbf72
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-24 13:23:28 -0500

    update file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,7 +6,8 @@ from sys import argv
     the value of the X-Request-Id variable found
     in the header of the response.
     -You must use the packages urllib and sys """
- 
+
+
 if __name__ == '__main__':
     with request.urlopen(argv[1]) as page:
         req_pag = page.headers.get("X-Request-Id")
