a3740d67f812c95ac9eafb70f6f258217b1295be
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-24 21:42:56 -0500

    update file
    script that takes your
    
    Github credentials (username and password)
    
    and uses the Github API to display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,7 +5,7 @@ from sys import argv
 
 if __name__ == '__main__':
     username = argv[1]
-    password = argv[2] 
+    password = argv[2]
     url = 'https://api.github.com/user'
     req = requests.get(url, auth=(username, password))
     print(req.json().get("id"))
